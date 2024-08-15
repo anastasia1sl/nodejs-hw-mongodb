c27c8584fb47ec837353d8ad900b8732b9f666b3
anastasia1sl <slobodyanik.a97@gmail.com>
2024-08-15 18:49:45 +0300

    homework

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,23 +9,38 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactsSchema,
+  updateContactsSchema,
+} from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
+
 const contactsRouter = Router();
 
 contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 
 contactsRouter.get(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(getContactByIdController),
 );
 
-contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+contactsRouter.post(
+  '/contacts',
+  validateBody(createContactsSchema),
+  ctrlWrapper(createContactController),
+);
 contactsRouter.patch(
   '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactsSchema),
   ctrlWrapper(patchContactController),
 );
 
 contactsRouter.delete(
   '/contacts/:contactId',
+  isValidId,
   ctrlWrapper(deleteContactController),
 );
 
